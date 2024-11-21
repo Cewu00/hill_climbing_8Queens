2d68e32373a90423edc699c8e1633747b8e83962
Vuk Djukanovic <81326012+Cewu00@users.noreply.github.com>
2024-11-21 02:44:39 +0100

    started working on basic functions that we will need in main so they can work as intermediaries between the visualisation and logic

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from visualisation import ChessBoardGUI
 from logic import ChessBoardLogic
 
+def set_board(gui:ChessBoardGUI, queen_positions:dict): # for logic only
+    if len(queen_positions) != gui.board_size:
+        raise Exception(f"Queen position lenght not matching board size {len(queen_positions)} != {gui.board_size}")
+    if all(x in queen_positions for x in range(gui.board_size)):
+        raise Exception(f"X values must be between 0 and {gui.board_size - 1}")
     
+    for i in range(gui.board_size):
+        y = queen_positions[i]
+
+def move_queen(self, x_position, new_y):
+    pass
+
 if __name__ == "__main__":    
     gui = ChessBoardGUI()
-    
     logic = ChessBoardLogic()
-    logic.print_chessboard()
+    
+    queen_positions = {}
+    
+    set_board(gui, queen_positions)
+    
     
     gui.mainloop()
