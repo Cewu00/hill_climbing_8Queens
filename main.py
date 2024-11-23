e3999a8dd23668a59fff4db13e7c32169c20648e
Vuk Djukanovic <81326012+Cewu00@users.noreply.github.com>
2024-11-23 20:38:24 +0100

    Poceo rad na vizuelizovanju algoritma

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from visualisation import ChessBoardGUI
 from logic import ChessBoardLogic
 
-def set_board(gui:ChessBoardGUI, queen_positions:dict): # for logic only
+after_time_tracker = 0
+
+def set_board(gui:ChessBoardGUI, queen_positions:dict): # for logic result only, queen_positions {x:y}
     if len(queen_positions) != gui.board_size:
         raise Exception(f"Queen position lenght not matching board size {len(queen_positions)} != {gui.board_size}")
-    if all(x in queen_positions for x in range(gui.board_size)):
+    if not all(x in queen_positions for x in range(gui.board_size)):
         raise Exception(f"X values must be between 0 and {gui.board_size - 1}")
-    
     for i in range(gui.board_size):
         y = queen_positions[i]
+        gui.draw_queen(i, y)
+        
 
-def move_queen(self, x_position, new_y):
-    pass
+def move_queen(gui:ChessBoardGUI, x:int, queen_positions:dict, new_y:int):
+    global after_time_tracker
+    y = queen_positions.pop(x)
+    queen_positions[x] = new_y
+    after_time_tracker += 1000
+    gui.move_queen(x, y, x, new_y)
+
+
+def hill_climbing_visualised():
+    return
+    
 
 if __name__ == "__main__":    
     gui = ChessBoardGUI()
     logic = ChessBoardLogic()
     
-    queen_positions = {}
+    
+    queen_positions = logic.queen_positions
     
     set_board(gui, queen_positions)
     
